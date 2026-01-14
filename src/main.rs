20135e1bb47c020af89d2ceff63b02b63efaa092
sud0pacman <mukhammad.kammoliddin@gmail.com>
2026-01-14 11:31:40 +0500

    inital state

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use std::convert::identity;
 
-use relm4::{
-    adw,
-    css, gtk::{
-        gdk::Texture,
-        gdk_pixbuf::Pixbuf,
-        gio::{Cancellable, MemoryInputStream},
-        glib,
-        prelude::*,
-    }, prelude::*
-};
+use relm4::{adw, gtk::prelude::*, prelude::*};
+
+mod dialog;
+use dialog::{AwesomeModel, DialogMsg};
 
 struct Header;
 
@@ -46,11 +40,10 @@ impl SimpleComponent for Header {
 
 #[derive(Debug)]
 enum AppMsg {
-    AwesomeModel, Close
+    AwesomeModel,
+    Close,
 }
 
-
-#[derive()]
 struct App {
     header: Controller<Header>,
     dialog: Controller<AwesomeModel>,
@@ -64,18 +57,31 @@ impl SimpleComponent for App {
 
     view! {
         #[root]
-        gtk::ApplicationWindow
+        adw::ApplicationWindow
         {
             set_default_size: (500, 250),
-            set_titlebar: Some(model.header.widget()),
 
             gtk::Box {
                 set_orientation: gtk::Orientation::Vertical,
+                // adw::HeaderBar {
+                //     // set_titlebar: Some(model.header.widget()),
 
-                gtk::LinkButton {
-                    set_label: "hi",
-                    connect_clicked => AppMsg::AwesomeModel,
+                // },
+                adw::ToolbarView {
+                    set_top_bar_style: adw::ToolbarStyle::Flat,
+
+                    add_top_bar = &adw::HeaderBar {
+                        // #[wrap(Some)]
+                        // set_title_widget = &adw::WindowTitle {
+                        //     set_title: "Wifi",
+                        // }
+                    },
+                    gtk::LinkButton {
+                        set_label: "hi",
+                        connect_clicked => AppMsg::AwesomeModel,
+                    },
                 },
+
             }
         }
     }
@@ -92,7 +98,6 @@ impl SimpleComponent for App {
         let dialog = AwesomeModel::builder()
             .launch(())
             .forward(sender.input_sender(), |_| AppMsg::Close);
-        
 
         let model = App { header, dialog };
 
@@ -109,141 +114,11 @@ impl SimpleComponent for App {
             AppMsg::Close => {
                 relm4::main_application().quit();
             }
-        }   
+        }
     }
 }
 
 fn main() {
     let app = RelmApp::new("relm4.example.components");
     app.run::<App>(());
-}
-
-fn embedded_logo() -> Texture {
-    let bytes = include_bytes!("../assets/logo.png");
-    let g_bytes = glib::Bytes::from(&bytes.to_vec());
-    let stream = MemoryInputStream::from_bytes(&g_bytes);
-    let pixbuf = Pixbuf::from_stream(&stream, Cancellable::NONE).unwrap();
-    Texture::for_pixbuf(&pixbuf)
-}
-
-
-use gettextrs::gettext;
-use relm4::adw::prelude::*;
-use relm4::gtk;
-
-#[derive(Debug)]
-pub enum DialogMsg {
-    Show,
-}
-
-#[derive(Debug, Clone)]
-pub struct AwesomeModel {
-    window: Option<adw::Dialog>,
-}
-
-#[relm4::component(pub)]
-impl SimpleComponent for AwesomeModel {
-    type Init = ();
-    type Input = DialogMsg;
-    type Output = ();
-
-    view! {
-        adw::Dialog {
-            set_title: &gettext("List of used e-imzo websites"),
-            set_follows_content_size: true,
-            set_presentation_mode: adw::DialogPresentationMode::Floating,
-
-            #[wrap(Some)]
-            set_child = &adw::ToolbarView {
-                add_top_bar = &adw::HeaderBar {},
-
-                #[wrap(Some)]
-                set_content = &gtk::Box {
-                    set_orientation: gtk::Orientation::Vertical,
-                    set_spacing: 12,
-                    set_margin_all: 24,
-                    
-                    gtk::Image {
-                        set_vexpand: true,
-                        set_hexpand: true,
-                        set_paintable: Some(&embedded_logo()),
-                        set_pixel_size: 125,
-                    },
-
-                    gtk::Label {
-                        set_label: "Fractal",
-                        add_css_class: "title-1",
-                    },
-
-                    gtk::Label {
-                        set_label: "Fraktal jamoasi",
-                    },
-
-                    gtk::Button {
-                        set_label: "13",
-                        add_css_class: "accent",
-                        add_css_class: "pill",
-                        set_halign: gtk::Align::Center,
-                        inline_css: "
-                            padding: 6px 20px; 
-                            min-height: 0px; 
-                            min-width: 0px;
-                            label { 
-                                margin: 0px; 
-                                padding: 0px; 
-                            }
-                        ",
-                    },
-
-                    gtk::ListBox {
-                        add_css_class: "boxed-list",
-                        set_margin_top: 20,
-
-                        adw::ActionRow {
-                            set_title: "Veb sayt",
-                            // set_activatable: true,
-                            // connect_activated => |_| {
-
-                            // },
-                            // set_subtitle: "Yagona interaktiv davlat xizmatlari portali",
-                            
-                            add_suffix = &gtk::LinkButton {
-                                set_uri: "https://my.gov.uz/uz",
-                                #[wrap(Some)]
-                                set_child = &gtk::Image {
-                                    set_icon_name: Some("external-link-symbolic"),
-                                    inline_css: "color: white;",
-                                }
-                            },  
-                        },
-                    },
-                },
-            },
-        }
-    }
-
-    fn init(
-        _init: Self::Init,
-        root: Self::Root,
-        _sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let model = AwesomeModel {
-            window: Some(root.clone()), 
-        };
-        
-        let widgets = view_output!();
-
-        ComponentParts { model, widgets }
-    }
-
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
-        match msg {
-            DialogMsg::Show => {
-                if let Some(dialog) = &self.window {
-                    let parent = relm4::main_application().active_window();
-                    dialog.present(parent.as_ref());
-                }
-            }
-        }
-    }
 }
